2a2f5bf315276bf1bca0853a3ee87704eb5c3552
Jacob Stoebel <jstoebel@villagemd.com>
2021-09-14 13:12:46 -0400

    start of pov

--- exercises/practice/pov/pov.py
+++ exercises/practice/pov/pov.py
@@ -1,8 +1,12 @@
 from json import dumps
+from typing import List, TypeVar, Optional, Generic
 
+class NodeNotFound(Exception):
+    pass
 
-class Tree:
-    def __init__(self, label, children=[]):
+TreeType = TypeVar('TreeType')
+class Tree(Generic[TreeType]):
+    def __init__(self, label: str, children: List["Tree[TreeType]"]=[]):
         self.label = label
         self.children = children
 
@@ -18,8 +22,37 @@ class Tree:
     def __eq__(self, other):
         return self.__dict__() == other.__dict__()
 
-    def from_pov(self, from_node):
+    def __repr__(self):
+        return f"<Tree '{self.label}'>"
+
+    def from_pov(self, from_node: str):
+        """
+        :param from_node: label of node to re-root
+        """
+        # find node
+        # that node becomes root
+        # for each relation (up or down):
+            # make a child
+            # repeat
         pass
 
-    def path_to(self, from_node, to_node):
+    def path_to(self, from_node: str, to_node: str):
+        """
+        :param from_node: label of node to start from
+        :param to_node: label of node to go to
+        :returns: ???
+        """
         pass
+
+    def find(self, target_node_label: str) -> "Tree[TreeType]":
+        """
+        :param node: label of node to find
+        :returns: node or raises NodeNotFound if not found
+        """
+
+        for node in self.children:
+            if node.label == target_node_label:
+                return node
+            return node.find(target_node_label)
+
+        raise NodeNotFound
